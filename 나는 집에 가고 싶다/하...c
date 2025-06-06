05fc7808467c42e08265b3e1cd69b499c623d999
USER <USER@DESKTOP-V73EH1A>
2025-06-06 22:01:14 +0900

    고양이와 수프 v1 불러오기

---
+++ 나는 집에 가고 싶다/하...c
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <time.h>
+
+#define ROOM_WIDTH 6
+#define HME_POS 1 //집 위치
+#define BWL_POS (ROOM_WIDTH - 2) //냄비 위치
+
+int main(void) {
+
+    printf("***야옹이와 스프***\n");
+    printf("  /\\_/\\ \n");
+    printf(" ( o.o )\n");
+    printf("<|  ^ <|\n");
+    printf(" |     |\\  \n");
+    printf("         \\/  \n");
+
+    printf("야옹이의 이름은 쫀덕입니다.");
+    Sleep(1000); //1초 대기
+    system("cls");
+
+
+
+    int soup = 0; //수프 개수
+    int relation = 2; //관계, 친밀도
+    int leave = 2; //이전 위치, 즉 이미 떠나 자리, 3이상이면 이동
+    int cat = 1; //고양이 위치
+    int a = 0; //주사위 수
+
+    while (1) {
+        printf("==========현재상태==========\n");
+        printf("지금까지 만든 수프의 개수: %d개\n", soup);
+        printf("집사와의 관계(0~4): %d\n", relation);
+
+        if (relation == 0) {
+            printf("곁에 오는 것조차 싫어합니다.\n");
+        }
+        else if (relation == 1) {
+            printf("간식 자판기 취급입니다.\n");
+        }
+        else if (relation == 2) {
+            printf("그럭저럭 쓸만한 집사입니다.\n");
+        }
+        else if (relation == 3) {
+            printf("훌륭한 집사로 인정받고 있습니다.\n");
+        }
+        else if (relation == 4) {
+            printf("집사 껌딱지입니다.\n");
+        }
+        else {
+            printf("잘못된 친밀도 값입니다.\n");
+        }
+        printf("===============================\n\n");
+        Sleep(500);
+
+        srand((unsigned)time(NULL));
+
+        printf("어떤상호작용을하시겠습니까?   0.아무것도하지않음 1.긁어주기>> ");
+        scanf_s("%d", &a);
+
+        while (a != 0 && a != 1) {
+
+            printf("0과 1중 하나를 입력하세요.\n");
+            printf("어떤상호작용을하시겠습니까?   0.아무것도하지않음 1.긁어주기>> ");
+            scanf_s("%d", &a);
+        }
+        if (a == 0) {
+
+            printf("아무것도 하지 않습니다.\n 4/6확룰로 친밀도가 떨어집니다.\n 주사위를 굴립니다. 또르륵...\n");
+            int r; //주사위 수
+            r = rand() % 6 + 1;
+            printf("%d이(가) 나왔습니다!\n", r);
+            if (r <= 4) { //주사위 눈 4이하 친밀도 하락
+                printf("친밀도가 떨어집니다.\n");
+                relation--;
+
+                if (relation < 0) {
+                    relation = 0; //친밀도는 0아래로 내려가면 안됨
+                }
+            }
+            else {
+                printf("다행히 친밀도가 떨어지지 않았습니다.\n");
+            }
+            printf("현재 친밀도: %d\n", relation);
+        }
+        else {
+            printf("쫀덕의 턱을 긁어주었습니다.\n 2/6확률로 친밀도가 높아집니다.\n 주사위를 굴립니다. 또르륵...\n");
+            int rr; //주사위 수
+            rr = rand() % 6 + 1;
+            printf("%d이(가) 나왔습니다!\n", rr);
+            if (rr >= 5) { //주사위 눈 5이상 친밀도 상승
+                printf("친밀도가 상승합니다.\n");
+                relation++;
+                if (relation > 4) {
+                    relation = 4; //친밀도는 4를 넘어갈 수 없음
+                }
+            }
+            else {
+                printf("친밀도는 그대로 입니다.\n");
+            }
+            printf("현재 친밀도: %d\n", relation);
+        }
+
+        for (int i = 0; i < ROOM_WIDTH; i++) {
+            printf("#");
+        }
+        printf("\n");
+
+        printf("#");
+        for (int i = 1; i < ROOM_WIDTH - 1; i++) {
+            if (i == HME_POS) {
+                printf("H"); //집
+            }
+            else if (i == BWL_POS) { //냄비
+                printf("B");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("#\n");
+
+        printf("#");
+        for (int i = 1; i < ROOM_WIDTH - 1; i++) {
+            if (i == cat) {
+                printf("C"); //고양이
+            }
+            else if (i == leave && cat != leave) {
+                printf(".");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("#\n");
+
+        for (int i = 0; i < ROOM_WIDTH; i++) {
+            printf("#");
+        }
+        printf("\n");
+
+        printf("쫀덕 이동: 집사와 친밀할 수록 냄비로 이동할 확률이 높아집니다.\n");
+        int limit = 6 - relation;
+        double probability = ((6 - limit + 1) / 6.0) * 100.0;
+        printf("주사위 눈이 %d이상이면 냄비쪽으로 이동합니다.\n", limit);
+        printf("→ 확률: 약 %.1f%%\n", probability);
+
+        srand((unsigned)time(NULL));
+
+        int dice = rand() % 6 + 1;
+        printf("주사위를 굴립니다. 또르륵...\n");
+        Sleep(1000);
+        printf("%d이(가) 나왔습니다!\n", dice);
+
+        if (dice >= limit) {
+            printf("냄비 쪽으로 이동합니다.\n\n");
+            if (cat < ROOM_WIDTH - 2) {
+                cat++;
+            }
+            else {
+                printf("하지만 벽에 막혀 움직일 수 없습니다.\n\n");
+            }
+        }
+        else {
+            printf("집 쪽으로 움직입니다.\n\n");
+            if (cat > 1) {
+                cat--;
+            }
+            else {
+                printf("하지만 벽에 막혀 움직일 수 없습니다.\n\n");
+            }
+        }
+        Sleep(1000);
+
+        for (int i = 0; i < ROOM_WIDTH; i++) printf("#");
+        printf("\n#");
+        for (int i = 1; i < ROOM_WIDTH - 1; i++) {
+            if (i == HME_POS) printf("H");
+            else if (i == BWL_POS) printf("B");
+            else printf(" ");
+        }
+        printf("#\n#");
+        for (int i = 1; i < ROOM_WIDTH - 1; i++) {
+            if (i == cat) {
+                printf("C");
+            }
+            else if (i == leave && cat != leave) {
+                printf(".");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("#\n");
+        for (int i = 0; i < ROOM_WIDTH; i++) printf("#");
+        printf("\n\n");
+
+        // 상태에 따른 이벤트
+        if (cat == HME_POS) {
+            printf("쫀떡은(는) 자신의 집에서 편안함을 느낍니다.\n\n");
+        }
+        else if (cat == BWL_POS) {
+            const char* soups[] = { "감자수프", "양송이수프", "브로콜리수프" };
+            int taste = rand() % 3;
+            soup++;
+            printf("쫀떡이(가) %s를 만들었습니다!\n", soups[taste]);
+            printf("현재까지 만든 수프: %d개\n\n", soup);
+        }
+    }
+
+    return 0;
+}
